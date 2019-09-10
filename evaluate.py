1b661c4d0e7b035c9cd9f3fe54e5b0337789b494
guixiangyu <guixiangyu1@gmail.com>
2019-09-10 11:24:15 +0800

    Initial commit

---
+++ evaluate.py
@@ -0,0 +1,37 @@
+import sys
+
+from model.data_utils import CoNLLDataset
+from model.ner_model import NERModel
+from model.config import Config
+
+
+
+def main():
+    # create instance of config
+    config = Config()
+
+    # build model
+    model = NERModel(config)
+    model.build("train")
+    model.restore_session(config.dir_model)
+
+    # create dataset
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'test':
+            test = CoNLLDataset(config.filename_test, config.processing_word,
+                                config.processing_tag, config.max_iter)
+        elif sys.argv[1] == 'dev':
+            test = CoNLLDataset(config.filename_dev, config.processing_word,
+                                config.processing_tag, config.max_iter)
+    else:
+        assert len(sys.argv) == 1
+        test = CoNLLDataset(config.filename_test, config.processing_word,
+                            config.processing_tag, config.max_iter)
+
+    # evaluate and interact
+    model.evaluate(test)
+    # interactive_shell(model)
+
+
+if __name__ == "__main__":
+    main()
